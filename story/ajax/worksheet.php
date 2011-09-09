a29fb578df71f8e5bb27cdb9c9ce06ee885d411a
Ben McMurry <benmcmurry@gmail.com>
2011-09-09 17:57:13 -0600

    Added Links to Pages on the Worksheet and changed its style.

--- story/ajax/worksheet.php
+++ story/ajax/worksheet.php
@@ -18,7 +18,7 @@ $link=connect(); //call function from external file to connect to database
 $user_id = $_SESSION['user_id'];
 $story = $_SESSION['story'];
 
-$query_worksheet = "Select * from Worksheet Where worksheet_story='$story' order by worksheet_order ASC"; //mysql query variable
+$query_worksheet = "Select * from Worksheet join Pages on Worksheet.worksheet_page = Pages.id Where Worksheet.worksheet_story='$story' order by Worksheet.worksheet_order ASC"; //mysql query variable
 $list_worksheet = mysql_query($query_worksheet) or die(mysql_error()); //execute query
 
 $query = "Select progress_page from User_Progress where progress_user = '$user_id' and progress_story='$story'";
@@ -40,23 +40,21 @@ story = <? echo $story; ?>;
 
 <div class="worksheet">
 <h2>Worksheet</h2>
-<table class="worksheet">
+
 
 <?
 while ($worksheet = mysql_fetch_assoc($list_worksheet)) {
 if ($worksheet['embedded'] == 1 && !in_array($worksheet['worksheet_page'], $visited_pages)){
-	echo "<tr class='sorry'>
-	<td colspan='2'>
-	You have not unlocked question ".$worksheet['worksheet_order'].".
-	<td>
-	</tr>";
+	echo "<div class='sorry'>
+	You have not unlocked question ".$worksheet['worksheet_order'].".</div>";
 
 }
 else {
 ?>
+<table class="worksheet">
 <tr>
 <td><? echo "<strong>".$worksheet['worksheet_type']."</strong><br />".$worksheet['worksheet_order'].". ".$worksheet['worksheet_text']."<br /><br />".$worksheet['worksheet_response']."";?></td>
-<td>
+<td class="treasure-chest">
 <?
 $query_lock = "SELECT * FROM User_Progress where progress_user = '".$user_id."' and progress_story='$story'"; //mysql query variable
 $list_lock = mysql_query($query_lock) or die(mysql_error()); //execute query
@@ -90,12 +88,15 @@ if (in_array($worksheet['worksheet_page'], $pages_visited)){echo "<img  id='".$w
 else {echo "<img class='answer-img closed' id='".$worksheet['worksheet_id']."' src='../img/closed.png' width='64px' />";}			
 //end icon printing
 ?></td>
-</tr><?
+</tr>
+<tr class="page-link"><td colspan="2"><? echo "Page: <a href='index.php?page_id={$worksheet['worksheet_page']}'>{$worksheet['page_name']}</a>";
+?></td></tr></table>
+<?
 }} while ($worksheet = mysql_fetch_assoc($list_worksheet));
 ?>
-
-<tr><td><a class='btn submit'>Submit Answers</a></td></tr>
-</table>
+<div class="submit">
+<a class='blockButton btn'>Submit Answers</a>
+</div>
 </div>
 <div class="page-instructions"><a class='page-instructions-toggle'> Use the 'i' key to toggle Instructions.</a>
 <p>This worksheet is like a worksheet that you need to complete by reading through the story.  You may answer the questions at any time.  You can unlock the correct answer by visiting the instructional page containing the answer to each question.  Click on the open treasure chest icon to find an explanation of the correct answer.</p>
