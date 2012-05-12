0b345204f35a2b2a94e8c4eff179301ff3541c3c
Ben McMurry <benmcmurry@gmail.com>
2012-05-11 22:01:47 -0600

    Revert "Lots of changes I didn't commit"
    This reverts commit 813a6c678cbdd4a6d00cda55e5fec9411d4ce4e8.

--- json.php
+++ json.php
@@ -2,21 +2,36 @@
 /* Depending on the url this provides absolute links to the files that are needed for every file. */
 $requestingURL = $_SERVER['SERVER_NAME'];
 if ($requestingURL == 'localhost') {
-	include_once("/Users/Ben/Sites/isee/authenticate.php");
 	include_once("/Users/Ben/Sites/connectFiles/connectProject301.php");
 	}
 else {
-	include_once("/home4/byuiptne/public_html/isee/authenticate.php");
 	include_once("/home4/byuiptne/connectFiles/connectProject301.php");
 	
 	}
 $link=connect(); //call function from external file to connect to database
 /* this is the end of the includes. */
-$page_parent = $_SESSION['current_page'];
-$id = $_POST['id'];
 
-$query = "DELETE FROM Page_Relations WHERE page_relation_id = '$id'";
-$run = mysql_query($query) or die(mysql_error());
+$query = "select id, page_name from Pages"; //mysql query variable
 
-echo $id." is deleted.";
+$run = mysql_query($query) or die(mysql_error()); //execute query
+
+while($results = mysql_fetch_assoc($run)) {
+
+	$r[] = $results;
+
+
+}
+
+
+$json = json_encode($r);
+
+echo $json;
+
+
+
+
+
+
+
+
 ?>
