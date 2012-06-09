81e0c9e6699f6fad342930dd000ed068bf9e2f41
Ben McMurry <benmcmurry@gmail.com>
2012-06-08 21:45:33 -0600

    localhost change for Ben's machine

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php 
 	function connect() {
 		$user="root";
-		$host="localhost";
+		$host="127.0.0.1";
 		$password="";
 		$database="dt1";
 		mysql_connect($host,$user,$password);
--- dashboard/index.php
+++ dashboard/index.php
@@ -1,6 +1,7 @@
 <?php
 
 $base_directory = dirname(dirname(__FILE__));
+echo $base_directory;
 include_once($base_directory."/connect.php");
 include_once($base_directory."/authenticate.php");
 $link=connect(); //call function from external file to connect to database
